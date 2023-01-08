d62deaef636774691f6689cac46ce4debcc295f1
aleksandr <aleksandrkrzhn@yandex.ru>
2023-01-09 01:41:45 +0400

    feat: use packer to create base image

---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Прерывать скрипт при ошибке
+set -e
+
+# Прерывать если не передана переменная
+set -u
+
+# Показывать команды
+set -x
+
+echo 'Обновить apt, предварительно убедившись что systemd не запустил свои задачи...'
+flock /var/lib/apt/daily_lock apt-get update
+
+echo 'Обновляем APT и устанавливаем Ruby и Bundler...'
+sudo apt-get install -y ruby-full ruby-bundler build-essential
+
+echo 'Готово'
