e0732e20954afcb54fbfb7905b5383afac30ecda
aleksandr <aleksandrkrzhn@yandex.ru>
2023-02-25 19:03:59 +0400

    fix: remove unsupported argument

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -29,8 +29,10 @@ resource "yandex_storage_bucket" "state-bucket" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
   bucket     = "tf-state-bucket-bucket"
-  max_size   = 10 * 1024 * 1024
   acl        = "private"
+
+  # Опция для более поздних версий
+#  max_size   = 10 * 1024 * 1024
 }
 
 output "bucket_access_key" {
